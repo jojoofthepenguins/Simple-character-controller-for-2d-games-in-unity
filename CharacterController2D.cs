eee73461008415c8d3f72f374217f6c498a4de00
wij <41879253+jojoofthepenguins@users.noreply.github.com>
2020-10-05 17:48:47 -0400

    Character controller

---
+++ CharacterController2D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+   
+    public float speed;
+
+    public float jump;
+
+    private float move;
+
+    private Rigidbody2D rb;
+
+    private bool isJumping;
+
+    private void Start()
+    {
+       rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+   private void Update()
+    {
+       move = Input.GetAxis("Horizontal");
+
+       rb.velocity = new Vector2(move * speed, rb.velocity.y);
+       if (Input.GetButtonDown("Jump") && !isJumping) 
+       {
+           rb.AddForce(new Vector2(rb.velocity.x, jump));
+           isJumping = true;
+       } 
+    }
+
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            isJumping = false;
+        }
+
+
+      
+   
+    }
+
+ 
+
+}
